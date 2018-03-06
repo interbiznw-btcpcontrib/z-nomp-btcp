6a9883a3838a282afbef95ac7487ad11d5953882
J62 <j.owens@interbiznw.com>
2018-03-05 20:23:10 -0800

    updated init

--- init.js
+++ init.js
@@ -501,8 +501,7 @@ var startProfitSwitch = function(){
         pools: JSON.stringify(poolConfigs),
         portalConfig: JSON.stringify(portalConfig)
     });
-web_worker = worker;
-    worker.on('exit', function(code, signal){
+        worker.on('exit', function(code, signal){
         logger.error('Master', 'Profit', 'Profit switching process died, spawning replacement...');
         setTimeout(function(){
             startWebsite(portalConfig, poolConfigs);
